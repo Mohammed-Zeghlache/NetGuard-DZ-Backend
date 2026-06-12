460a877c909ea8d49c1fc37c1b54d97dd62b3a7c
Mohammed-Zeghlache <mouhamedzeghlache7@gmail.com>
2026-06-12 20:26:47 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -24,9 +24,10 @@ const pool = new Pool({
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
-  ssl: process.env.DB_HOST === 'localhost' 
-    ? false 
-    : { require: true, rejectUnauthorized: false }
+  ssl: {
+    require: true,
+    rejectUnauthorized: false
+  }
 });
 
 
